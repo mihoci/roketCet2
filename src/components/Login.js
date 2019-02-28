073cc54fc8b5fd47256853a9a38d631f61ab90ed
marta <marta.pintaric2@gmail.com>
2019-02-28 08:29:52 +0100

    login design

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,46 +1,66 @@
 import React from 'react'
+import img from '../618.png'
+import Tilt from 'react-tilt'
 
-class Login extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            username: '',
-            password: ''
-        }
-    }
 
-    handleLogin(event){
-        event.preventDefault();
-        this.props.setUser(this.state.username)
-    }
+class Login extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			username: '',
+			password: ''
+		}
+	}
 
-    render(){
-        return(
-            <div className="limiter">
-                <form onSubmit={this.handleLogin.bind(this)}>
-                    <label>
-                        username: 
-                        <input 
-                            type="text" 
-                            value={this.state.username}
-                            onChange={(e) => this.setState({username: e.target.value})}
-                        />
-                    </label>
-                    {/* <br />
-                    <label>
-                        password:
-                        <input 
-                            type="password" 
-                            value={this.state.password}
-                            onChange={(e) => this.setState({password: e.target.value})}
-                        />
-                    </label> */}
-                    <br />
-                    <input type="submit" value="Submit" />
-                </form>
-            </div>
-        )
-    }
+	handleLogin(event) {
+		event.preventDefault();
+		this.props.setUser(this.state.username)
+	}
+
+	render() {
+		return (
+			<div className="limiter">
+				<div className="container-login">
+					<div className="wrap-login">
+						<div className="row">
+							<div className="col-md-6 leftDesign">
+								<div className="cadre-login">
+									<Tilt className="Tilt" options={{ max: 35 }} style={{ height: 177, }} >
+										<p className="naslov-login">ROKET ČET</p>
+										<img className="login-img" src={img} />
+										<p className="login-text">TIM SPARTA</p>
+									</Tilt>
+								</div>
+							</div>
+							<div className="col-md-6">
+								<div className="cadre-login-right">
+									<form onSubmit={this.handleLogin.bind(this)}>
+										<p className="naslov-login-ime">
+											ROKET ČET
+									</p>
+										<p className="naslov-login-poruka">
+											Member Login
+									</p>
+										<input className="form-control-login" placeholder="Username" type="text" value={this.state.username} onChange={(e) => this.setState({
+											username: e.target.value
+										})} />
+										<span className="focus-input100"></span>
+										<div className="container-login100-form-btn">
+											<button className="btn-login" type="submit" value="Submit">
+												Login
+												</button>
+										</div>
+
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		)
+	}
 }
 
 export default Login
